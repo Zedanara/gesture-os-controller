2686fc25760aa0399182af212cf81aece45ef1f4
Ivan <s181426@student.uken.krakow.pl>
2026-07-05 22:37:23 +0200

    feat: map swipe actions to standard VK_LEFT and VK_RIGHT keys for seamless slide navigation

--- host/src/os_control.cpp
+++ host/src/os_control.cpp
@@ -2,29 +2,21 @@
 #include <cstdio>
 #include <windows.h>
 
-static void send_media_key(WORD vk_code) {
+static void send_keyboard_key(WORD vk_code) {
     INPUT inputs[2] = {0};
-
     inputs[0].type = INPUT_KEYBOARD;
     inputs[0].ki.wVk = vk_code;
-
     inputs[1].type = INPUT_KEYBOARD;
     inputs[1].ki.wVk = vk_code;
     inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
-
     SendInput(2, inputs, sizeof(INPUT));
 }
 
 void os_execute_action(const GestureEvent* event) {
     switch (event->gesture_id) {
-        case 0x10:
-            printf("[ACTION] DOUBLE WAVE -> Simulating PLAY/PAUSE media key\n");
-            send_media_key(VK_MEDIA_PLAY_PAUSE);
-            break;
-
         case 0x20:
-            printf("[ACTION] SHORT HOLD -> Simulating VOLUME UP\n");
-            send_media_key(VK_VOLUME_UP);
+            printf("[ACTION] SHORT HOLD -> PLAY / PAUSE\n");
+            send_keyboard_key(VK_MEDIA_PLAY_PAUSE);
             break;
 
         case 0x21:
@@ -32,8 +24,27 @@ void os_execute_action(const GestureEvent* event) {
             LockWorkStation();
             break;
 
+        case 0x30:
+            printf("[ACTION] SWIPE IN -> Next Slide (KEY RIGHT)\n");
+            send_keyboard_key(VK_RIGHT);
+            break;
+
+        case 0x31:
+            printf("[ACTION] SWIPE OUT -> Previous Slide (KEY LEFT)\n");
+            send_keyboard_key(VK_LEFT);
+            break;
+
+        case 0x40:
+            printf("[ACTION] SLIDER -> Volume UP\n");
+            send_keyboard_key(VK_VOLUME_UP);
+            break;
+
+        case 0x41:
+            printf("[ACTION] SLIDER -> Volume DOWN\n");
+            send_keyboard_key(VK_VOLUME_DOWN);
+            break;
+
         default:
-            printf("[WARN] Unknown gesture ID: 0x%02X\n", event->gesture_id);
             break;
     }
 }
